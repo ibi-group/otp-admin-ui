60c72bc31f76f84ad1576f8ec7b5b5cad76e5bcc
Landon Reed <landontreed@gmail.com>
2020-03-19 17:06:03 -0400

    refactor(auth0): fix logout after bad login attempt

--- pages/oops.js
+++ pages/oops.js
@@ -1,10 +1,21 @@
 import React from 'react'
 import { useRouter } from 'next/router'
+import { useAuth } from 'use-auth0-hooks'
+
+import { logout } from '../util/auth'
 
 export default function Oops () {
-  const router = useRouter()
-  const { message } = router.query
-
+  const { asPath, query } = useRouter()
+  const { error } = useAuth({
+    audience: process.env.AUTH0_AUDIENCE,
+    scope: process.env.AUTH0_SCOPE
+  })
+  // There appears to be an issue with use-auth0-hooks, where logout does not
+  // occur properly if an unauthorized user attempts login. Without the
+  // logout invocation below, an attempt to re-login would not show the
+  // login widget to the user for them to try again.
+  if (error) logout(asPath)
+  const { message } = query
   return (
     <div>
       <h1>Oops</h1>
