72ae2a33675a37e0b291037bd483a24f81ccd81b
Landon Reed <landontreed@gmail.com>
2020-09-01 14:39:19 -0400

    refactor(Footer): improve comment

--- components/Footer.js
+++ components/Footer.js
@@ -13,7 +13,9 @@ export default function Footer (props) {
           <ul>
             <li>
               <NavLink
-                // FIXME: Update content at link OR change to Quick Start.
+                // FIXME: Update content at link OR perhaps replace with some
+                // Quick Start/Getting Started docs that could help out a new
+                // API user.
                 href='/about'>
                 About this app
               </NavLink>
