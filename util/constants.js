e3c5530b84050e73605b1adc1e5c4f3c59c143d1
binh-dam-ibigroup <56846598+binh-dam-ibigroup@users.noreply.github.com>
2021-04-15 12:56:05 -0400

    fix(util/constants): Fix scope syntax.

--- util/constants.js
+++ util/constants.js
@@ -1,4 +1,5 @@
-export const AUTH0_SCOPE = 'admin-user,api-user'
+// Auth0 scopes must now be separated by spaces (not commas).
+export const AUTH0_SCOPE = 'admin-user api-user'
 
 export const ADMIN_USER_URL = `${process.env.API_BASE_URL}/api/admin/user`
 export const API_USER_URL = `${process.env.API_BASE_URL}/api/secure/application`
