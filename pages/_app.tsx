db81822f518ab7be468d735bbd714fe4e04bef85
miles-grant-ibi <miles.grant@ibigroup.com>
2022-04-01 11:49:19 -0700

    refactor: remove duplicated font definitions

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -94,15 +94,6 @@ export default class Root extends App {
       >
         <LayoutWithAuth0>
           <Component {...pageProps} />
-          <style global jsx>
-            {`
-              * {
-                font-family: -apple-system, BlinkMacSystemFont, avenir next,
-                  avenir, segoe ui, helvetica neue, helvetica, Ubuntu, roboto,
-                  noto, arial, sans-serif;
-              }
-            `}
-          </style>
         </LayoutWithAuth0>
       </Auth0Provider>
     )
