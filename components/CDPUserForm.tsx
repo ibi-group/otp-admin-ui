5375f8a4d1f3736d6fe8ea63448fbc34476b0819
miles-grant-ibi <miles.grant@ibigroup.com>
2022-04-06 07:59:51 -0700

    refactor: remove name update form

--- components/CDPUserForm.tsx
+++ components/CDPUserForm.tsx
@@ -23,22 +23,6 @@ const CDPUserForm = ({ cdpUser, isSelf, onUpdateUser }: Props): JSX.Element => {
     <div>
       <p>Account type: CDP</p>
       <p>Email: {cdpUser.email}</p>
-      <Alert variant="warning">
-        <Alert.Heading>Example Only!</Alert.Heading>
-        <p>
-          This demonstrates that this field can be changed, but is only a demo.
-          If we want to edit more than one character at a time, we should use
-          Formik (see ApiUserForm)
-        </p>
-      </Alert>
-      <Form.Label htmlFor="name">Name</Form.Label>
-      <Form.Control
-        defaultValue={cdpUser.name}
-        disabled={!onUpdateUser}
-        id="name"
-        name="name"
-        onChange={(e) => updateUser({ name: e.target.value })}
-      />
     </div>
   )
 }
