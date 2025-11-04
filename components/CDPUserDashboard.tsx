a67e2c9bffa87f6c7e853426eaad56bb0aaa800b
binh-dam-ibigroup <binh.dam@arcadis.com>
2025-11-04 14:45:15 -0500

    fix(CDPUserDashboard): Handle no files.

--- components/CDPUserDashboard.tsx
+++ components/CDPUserDashboard.tsx
@@ -151,9 +151,9 @@ const CDPUserDashboard = (props: Props): JSX.Element => {
     fetchData()
   }, [auth0, currentlyDownloadingFile])
 
-  let files = Object.keys(swrData).length > 0 ? swrData?.data?.data : []
+  let files = Object.keys(swrData).length > 0 ? (swrData?.data?.data || []) : []
   files = files
-    ?.filter((file: CDPFile) => file?.size > 0)
+    .filter((file: CDPFile) => file?.size > 0)
     // Negative sorts "reverse alphabetically" which allows newest files to appear first
     .sort((a: CDPFile, b: CDPFile) => -a.key.localeCompare(b.key))
 
