d6004f3df5995f9a0bd764c3f3b0f7ac9a922884
binh-dam-ibigroup <binh.dam@arcadis.com>
2025-11-10 16:53:57 -0500

    fix(DailyStatsChart): Correctly compute ranges.

--- components/DailyStatsChart.tsx
+++ components/DailyStatsChart.tsx
@@ -47,20 +47,28 @@ class DailyStatsChart extends Component<Props, { value: GraphValue | null }> {
    */
   _getSeries = (series: keyof StatsRecord) => {
     let rangeMax = 0
+    let startDateMillis = Number.MAX_VALUE
+    let endDateMillis = 0
     const ONE_DAY_MILLIS = 86400000
     // Format request data for chart component.
+    // End the x range one millisecond before so that the bar
+    // doesn't bleed into the next day and the chart doesn't associate the bar with the next day.
     const chartData: RectSeriesPoint[] =
       this.props.records.map((value, i) => {
         const begin = value.date.valueOf()
         const y = value[series] || 0
-        const end = begin + ONE_DAY_MILLIS
+        const end = begin + ONE_DAY_MILLIS - 1
         if (y > rangeMax) rangeMax = y
+        if (startDateMillis > begin) startDateMillis = begin
+        if (endDateMillis < begin) endDateMillis = begin
         return { x: end, x0: begin, y, y0: 0 }
       }) || []
 
       return {
         chartData,
+        endDateMillis,
         rangeMax,
+        startDateMillis
       }
   }
 
@@ -76,13 +84,11 @@ class DailyStatsChart extends Component<Props, { value: GraphValue | null }> {
     const days = 30
     const { value } = this.state
     const ONE_DAY_MILLIS = 86400000
-    const startDate = moment(this.props.records[0].date)
 
     // Render the # of requests per API key on each day beginning
     // with the start date.
-    const otpUsersSeries = this._getSeries(this.props.series)
-    const timestamp = startDate.valueOf()
-    let rangeMax = otpUsersSeries.rangeMax
+    const seriesData = this._getSeries(this.props.series)
+    const { chartData, endDateMillis, rangeMax, startDateMillis } = seriesData
     const maxY = rangeMax === 0 ? 10 : Math.ceil(rangeMax / 10) * 10
     return (
       <div className="usage-list" style={{ display: 'inline-block' }}>
@@ -92,8 +98,9 @@ class DailyStatsChart extends Component<Props, { value: GraphValue | null }> {
           style={{ overflow: 'initial' }}
           width={600} // Round up max y value to the nearest 10
           xDomain={[
-            timestamp - 2 * ONE_DAY_MILLIS,
-            timestamp + 30 * ONE_DAY_MILLIS
+            startDateMillis - 2 * ONE_DAY_MILLIS,
+            // Display at least 30 days if a short time window was provided.
+            Math.min(startDateMillis + days * ONE_DAY_MILLIS, endDateMillis + 2 * ONE_DAY_MILLIS)
           ]}
           yDomain={[0, maxY]}
         >
@@ -102,7 +109,7 @@ class DailyStatsChart extends Component<Props, { value: GraphValue | null }> {
           <XAxis tickFormat={(d) => moment(d).format('MMM DD')} />
           <YAxis />
           <VerticalRectSeries
-            data={otpUsersSeries.chartData}
+            data={chartData}
             onValueMouseOut={this.handleClearValue} // Update value on mouse over/out.
             onValueMouseOver={this.handleSetValue}
             style={{ stroke: '#fff' }}
